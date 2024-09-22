d7b98900004a4c5d204038673e6f7e78ce6dfd8c
HoDucTriHieu <hoductrihieu@gmail.com>
2024-09-22 16:17:12 -0700

    week4 reactnative expo screen3 update1

--- Screen3.js
+++ Screen3.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   Text,
   SafeAreaView,
@@ -6,30 +6,38 @@ import {
   View,
   Button,
   Image,
-} from "react-native";
-import { Card } from "react-native-paper";
+  TouchableOpacity,
+} from 'react-native';
+import { Card } from 'react-native-paper';
 
 export default function App() {
   const [price, setPrice] = useState([]);
 
   return (
-    <View style={{ padding: 25 }}>
+    <View
+      style={{
+        paddingTop: 40,
+        paddingLeft: 2,
+        paddingRight: 2,
+        flex: 1,
+        marginTop: 20,
+      }}>
       <View style={styles.header}>
-        <View>
-          <Image style={styles.image} source={require("./assets/anh3.png")} />
+        <View style={{ flex: 1, justifyContent: 'space-between' }}>
+          <Image style={styles.image} source={require('./assets/anh3.png')} />
+          <Text>Mã giảm giá đã lưu</Text>
         </View>
         <View
           style={{
             flex: 1,
-            justifyContent: "space-between",
-            flexDirection: "column",
-          }}
-        >
+            justifyContent: 'space-between',
+            flexDirection: 'column',
+          }}>
           <View
             style={{
               flex: 3,
-            }}
-          >
+              justifyContent: 'space-between',
+            }}>
             <Text style={styles.title_header}>
               Nguyên hàm tích phân và ứng dụng
             </Text>
@@ -37,48 +45,148 @@ export default function App() {
             <Text style={styles.price}>141.800 đ</Text>
             <Text style={styles.price_discount}>141.800 đ</Text>
           </View>
-          <View style={{ flex: 1, justifyContent: "space-between" }}></View>
+          <View>
+            <Text>Xem tại đây</Text>
+          </View>
         </View>
       </View>
-      <View>
-        <Text>Mã giảm giá đã lưu</Text>
-        <Text>Mã giảm giá đã lưu</Text>
+      <View style={styles.viewMaGG}>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'flex-start',
+            flexDirection: 'row',
+            height: 45,
+            borderWidth: 1,
+          }}>
+          <View
+            style={{
+              marginLeft: 20,
+              backgroundColor: '#F2DD1B',
+              width: 32,
+              height: 16,
+            }}></View>
+          <Text style={{ marginLeft: 20 }}>Mã giảm giá</Text>
+        </View>
+        <View
+          style={{
+            width: 99,
+            height: 45,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <TouchableOpacity
+            style={[
+              styles.button,
+              { backgroundColor: '#0A5EB7', width: 99, height: 45, borderRadius: 0, marginLeft:'30%' },
+            ]}>
+            <Text style={styles.buttonText}>Áp dụng</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={{ flex: 0.1, backgroundColor: '#C4C4C4' }}></View>
+      <View style={styles.style_ttinh_ttien}>
+        <Text style={styles.txtQuaTang}>
+          Bạn có phiếu quà tặng Tiki/Got it/ Urbox?
+        </Text>
+        <Text
+          style={[styles.txtQuaTang, { color: '#134FEC', marginLeft: '7%' }]}>
+          Nhập tại đây?
+        </Text>
+      </View>
+      <View style={{ flex: 0.1, backgroundColor: '#C4C4C4' }}></View>
+      <View style={styles.style_ttinh_ttien}>
+        <Text style={[styles.txtQuaTang, { fontSize: 18 }]}>Tạm tính:</Text>
+        <Text style={styles.price}>141.800 đ</Text>
+      </View>
+
+      <View style={{ flex: 1.5, backgroundColor: '#C4C4C4' }}></View>
+      <View style={{ flex: 1, flexDirection: 'column' }}>
+        <View style={styles.style_ttinh_ttien}>
+          <Text style={[styles.txtQuaTang, { fontSize: 18, color: 'gray' }]}>
+            Thành tiền
+          </Text>
+          <Text style={styles.price}>141.800 đ</Text>
+        </View>
+        <View style={{ flex: 1, alignItems: 'center' }}>
+          <TouchableOpacity
+            style={[styles.button, { backgroundColor: '#E53935' }]}>
+            <Text style={styles.buttonText}>TIẾN HÀNH ĐẶT HÀNG</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  button: {
+    height: 45,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 331,
+    borderRadius: 5,
+  },
+  viewMaGG: {
+    flex: 0.5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    // marginTop: 20,
+    marginLeft: '7%',
+    marginRight: '7%',
+    paddingTop: '20%',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: '700',
+  },
   image: {
     width: 104,
     height: 127,
   },
   header: {
+    marginLeft: '7%',
+    marginRight: '7%',
     flex: 1,
-
-    justifyContent: "space-evenly",
-    flexDirection: "row",
+    justifyContent: 'space-between',
+    flexDirection: 'row',
   },
   title_header: {
-    fontFamily: "Roboto",
+    fontFamily: 'Roboto',
     fontSize: 12,
     fontWeight: 700,
   },
   title2_header: {
-    fontFamily: "Roboto",
+    fontFamily: 'Roboto',
     fontSize: 12,
     fontWeight: 700,
   },
   price: {
-    fontFamily: "Roboto",
+    fontFamily: 'Roboto',
     fontSize: 18,
     fontWeight: 700,
-    color: "#EE0D0D",
+    color: '#EE0D0D',
   },
   price_discount: {
-    fontFamily: "Roboto",
+    fontFamily: 'Roboto',
     fontSize: 12,
     fontWeight: 700,
-    color: "#808080",
+    color: '#808080',
+    textDecorationLine: 'line-through',
+  },
+  txtQuaTang: {
+    fontFamily: 'Roboto',
+    fontSize: 12,
+    fontWeight: 700,
+  },
+  style_ttinh_ttien: {
+    flex: 0.5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginLeft: '7%',
+    marginRight: '7%',
   },
 });
